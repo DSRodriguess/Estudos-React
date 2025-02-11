fc2c2fe70d30d86882b40bcfd0641306717af449
David da Silva Rodrigues <rodrigues.david@graduao.uerj.br>
2025-02-11 16:37:02 -0300

    Atualização nos times

--- src/App.js
+++ src/App.js
@@ -7,35 +7,40 @@ function App() {
 
   const times = [
     {
-      nome: 'Mid',
+      nome: 'Programação',
       corPrimaria: '#57C278',
       corSecundaria: '#D9F7E9'
     },
     {
-      nome: 'Top',
+      nome: 'Front-End',
       corPrimaria: '#82CFFA',
       corSecundaria: '#E8F8FF'
     },
     {
-      nome: 'Jungle',
+      nome: 'Data Science',
       corPrimaria: '#A6D157',
       corSecundaria: '#F0F8E2'
     },
     {
-      nome: 'AD Carry',
+      nome: 'Devops',
       corPrimaria: '#E06B69',
       corSecundaria: '#FDE7E8'
     },
     {
-      nome: 'Support',
+      nome: 'UX e Design',
       corPrimaria: '#DB6EBF',
       corSecundaria: '#FAE9F5'
     },
     {
-      nome: 'Staff',
+      nome: 'Mobile',
       corPrimaria: '#FFBA05',
       corSecundaria: '#FFF5D9'
     },
+    {
+      nome: 'Inovação e Gestão',
+      corPrimaria: '#FF8A29',
+      corSecundaria: '#FFEEDF'
+    }
   ]
 
   const [colaboradores, setColaboradores] = useState([])
